53604b4028c9eae44b73ef1b06beb592bc188771
Sun.Dev <154416623+SunMasterDev@users.noreply.github.com>
2025-05-12 04:17:52 +0700

    update card

--- src/components/CardStatusPreview/CardStatus.jsx
+++ src/components/CardStatusPreview/CardStatus.jsx
@@ -8,21 +8,21 @@ const CardStatus = () => {
         { label: 'QUERIES',    value: '12M+' },
       ];
   return (
-    <Card className="flex flex-col-reverse text-center md:flex-row overflow-hidden max-w-[90rem] bg-desaturatedBlue text-white">
-        <CardContent className="flex-1 p-10 md:p-20 space-y-6 md:text-left">
-            <h1 className="text-4xl md:text-5xl font-bold font-display">
+    <Card className="flex flex-col-reverse text-center md:flex-row overflow-hidden max-w-[76rem] bg-desaturatedBlue text-white">
+        <CardContent className="flex-1 p-4 md:p-16 space-y-6 md:text-left">
+            <h1 className="text-3xl md:text-4xl font-bold font-display">
                  Get <span className="text-softViolet">insights</span> that help your business grow.
             </h1>
-            <p className="text-base md:text-xl text-white/75 md:pt-6">
+            <p className="text-base md:text-base text-white/75 md:pt-6">
                 Discover the benefits of data analytics and make <br /> better decisions regarding revenue, customer <br />experience, and overall efficiency.
             </p>
             {/* Status */}
-            <div className="flex flex-col md:flex-row gap-6 pt-10 md:pt-20 justify-between">
+            <div className="flex flex-col md:flex-row gap-6 pt-4 md:pt-10 justify-between">
                 {
                     stats.map((items,index)=>(
                         <div key={index}>
-                            <h2 className="text-2xl md:text-4xl font-bold font-display">{items.value}</h2>
-                            <p className="text-base tracking-wide text-white/60 mt-2">{items.label}</p>
+                            <h2 className="text-2xl md:text-2xl font-bold font-display">{items.value}</h2>
+                            <p className="text-sm tracking-wide text-white/60 mt-2">{items.label}</p>
                         </div>
                     ))
                 }
